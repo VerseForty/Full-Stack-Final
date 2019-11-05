07534c92a970421af9187377212ed76f1d7ecf4c
msvarelli <56646972+msvarelli@users.noreply.github.com>
2019-11-05 17:50:15 -0500

    Add files via upload

---
+++ Frontend/characterCreator/javascript/formToJSON.js
@@ -0,0 +1,23 @@
+
+
+function makeJSON(){
+  var name = document.forms["characterForm"]["characterName"].value;
+  //alert(name);
+  //alert(typeof name);
+  var race = document.forms["characterForm"]["race"].value;
+  var playerClass = document.forms["characterForm"]["class"].value;
+  var strength = document.forms["characterForm"]["strength"].value;
+  var dexterity = document.forms["characterForm"]["dexterity"].value;
+  var constitution = document.forms["characterForm"]["constitution"].value;
+  var intelligence = document.forms["characterForm"]["intelligence"].value;
+  var wisdom = document.forms["characterForm"]["wisdom"].value;
+  var charisma = document.forms["characterForm"]["charisma"].value;
+
+
+  //alert(name + race + playerClass + strength, dexterity, constitution, intelligence, wisdom, charisma);
+
+  characterSheetObj = {name: name, race: race, class: playerClass, strength: strength, dexterity: dexterity, constitution: constitution, intelligence: intelligence, wisdom: wisdom, charisma: charisma}
+  characterSheetJSON = JSON.stringify(characterSheetObj);
+//?  localStorage.setItem("testJSON", characterSheetJSON);
+  alert(characterSheetJSON);
+}
