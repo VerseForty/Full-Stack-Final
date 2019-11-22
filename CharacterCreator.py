1daec4bab558bdd0ccbde84894ea02a94144984e
ChrissCruz <48289149+ChrissCruz@users.noreply.github.com>
2019-11-22 08:47:30 -0500

    Update

--- CharacterCreator.py
+++ CharacterCreator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[35]:
 
 
 class character:
@@ -30,21 +30,21 @@ class monster:
         
 
 
-# In[6]:
+# In[102]:
 
 
 c = character("Chris", 10, 10, 10, 11, 10, 10, 10, 3, 30)
 c.name
 
 
-# In[7]:
+# In[103]:
 
 
 m = monster(1, "Ghoul", 14, 12, "Bite" )
 m.name
 
 
-# In[8]:
+# In[104]:
 
 
 import random
@@ -52,7 +52,7 @@ club = random.randint(1,6)
 print(club)
 
 
-# In[9]:
+# In[105]:
 
 
 def rollToHit(character):
@@ -62,16 +62,52 @@ def rollToHit(character):
     return accuracy + prof
 
 
-# In[10]:
+# In[106]:
 
 
 def proficiency(character):
     if character.level == 3:
+        print("Hi")
         return 3
-    
+    elif character.level == 4:
+        print("yo")
+        return 4
+    elif character.level == 5:
+        print("yoda")
+        return 5
+    elif character.level == 6:
+        return 6
+    elif character.level == 7:
+        return 7
+    elif character.level == 8:
+        return 8
+    elif character.level == 9:
+        return 9
+    elif character.level == 10:
+        return 10
+    elif character.level == 11:
+        return 11
+    elif character.level == 12:
+        return 12
+    elif character.level == 13:
+        return 13
+    elif character.level == 14:
+        return 14
+    elif character.level == 15:
+        return 15
+    elif character.level == 16:
+        return 16
+    elif character.level == 17:
+        return 17
+    elif character.level == 18:
+        return 18
+    elif character.level == 19:
+        return 19
+    elif character.level == 20:
+        return 20
 
 
-# In[12]:
+# In[107]:
 
 
 def Battle(character, monster):
@@ -86,7 +122,7 @@ def Battle(character, monster):
         
 
 
-# In[13]:
+# In[108]:
 
 
 def Battle1(character, monster):
@@ -98,15 +134,29 @@ def Battle1(character, monster):
         return 0
 
 
-# In[14]:
+# In[111]:
 
 
 def attack1(character):
     if character.strength == 11:
         return 1
+    elif character.strength == 12:
+        return 2
+    elif character.strength == 13:
+        return 3
+    elif character.strength == 14:
+        return 5
+    elif character.strength == 15:
+        return 7
+    elif character.strength == 16:
+        return 10
+    elif character.strength == 17:
+        return 13
+    elif character.strength == 18:
+        return 17
 
 
-# In[15]:
+# In[112]:
 
 
 Battle(c,m)
