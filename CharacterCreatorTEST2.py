700d9c6448d8937970d8dc72aed83f2269e7796b
ChrissCruz <48289149+ChrissCruz@users.noreply.github.com>
2019-12-03 14:05:23 -0500

    Create CharacterCreatorTEST2.py

---
+++ CharacterCreatorTEST2.py
@@ -0,0 +1,426 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class character:
+    def __init__(characterC, name, AC, dexterity, constitution, strength, intelligence, wisdom, charisma, level, HP, Class, feature,weapon):
+        characterC.name = name
+        characterC.AC = AC
+        characterC.dexterity = dexterity
+        characterC.constitution = constitution
+        characterC.strength = strength
+        characterC.intelligence = intelligence
+        characterC.wisdom = wisdom
+        characterC.charisma = charisma
+        characterC.level = level
+        characterC.HP = HP
+        characterC.Class = Class
+        characterC.feature = feature
+        characterC.weapon = weapon
+
+
+        
+class monster:
+    def __init__(mon, CR, name, AC, HP, ability):
+        mon.CR = CR
+        mon.name = name
+        mon.AC = AC
+        mon.HP = HP
+        mon.ability = ability
+        
+    
+class fighter:
+    def __init__(fighterC, level, BaseAttackBonus, Fortitude, Reflex, Will, Special):
+        fighterC.level = level
+        fighterC.BaseAttackBonus = BaseAttackBonus
+        fighterC.Fortitude = Fortitude
+        fighterC. Reflex = Reflex
+        fighterC.Will = Will
+class rogue:
+    def __init__(rogueC, level, BaseAttackBonus, Fortitude, Reflex, Will, Special):
+        rogueC.level = level
+        rogueC.BaseAttackBonus = BaseAttackBonus
+        rogueC.Fortitude = Fortitude
+        rogueC.Reflex = Reflex
+        rogueC.Will = Will
+       
+        
+        
+
+
+# In[25]:
+
+
+c = character("Chris", 10, 10, 10, 11, 10, 10, 10, 3, 30,"Fighter","Dodge","Club")
+
+f = fighter(0,0,0,0,0,0)
+
+r = character("Rick",  10, 11, 10, 10, 10, 10, 10, 3,30,"Rogue","Dodge","Club")
+    
+
+
+# In[26]:
+
+
+m = monster(1, "Ghoul", 14, 12, "Bite" )
+m.name
+
+c.name
+r.dexterity
+
+
+# In[ ]:
+
+
+
+
+
+# In[27]:
+
+
+def rollToHit(character):
+    accuracy = random.randint(1,20)
+    prof = proficiency(character)
+    
+    return accuracy + prof
+
+
+# In[28]:
+
+
+def proficiency(character):
+    if character.level == 3:
+        print("Hi")
+        return 3
+    elif character.level == 4:
+        print("yo")
+        return 4
+    elif character.level == 5:
+        print("yoda")
+        return 5
+    elif character.level == 6:
+        return 6
+    elif character.level == 7:
+        return 7
+    elif character.level == 8:
+        return 8
+    elif character.level == 9:
+        return 9
+    elif character.level == 10:
+        return 10
+    elif character.level == 11:
+        return 11
+    elif character.level == 12:
+        return 12
+    elif character.level == 13:
+        return 13
+    elif character.level == 14:
+        return 14
+    elif character.level == 15:
+        return 15
+    elif character.level == 16:
+        return 16
+    elif character.level == 17:
+        return 17
+    elif character.level == 18:
+        return 18
+    elif character.level == 19:
+        return 19
+    elif character.level == 20:
+        return 20
+
+
+# In[29]:
+
+
+def Battle(character, monster):
+    
+    monsterHP = monster.HP
+    
+    characterHP = character.HP
+    
+    while monsterHP > 0 or characterHP > 0:
+        
+        RTH = input("Roll to his? Type YES or NO")
+        print(RTH)
+        if(RTH == "YES"):
+        
+            z = Battle1(character, monster)
+            
+            x = monsterHP - z
+            
+            monsterHP = x
+            
+            print(monsterHP)
+            if(monsterHP <= 0):
+                break
+            
+            zz = monBattle(character,monster)
+            
+            xx = characterHP - zz
+            
+            characterHP = xx
+            print(characterHP)
+            
+            
+        elif(RTH == "NO"):
+            print("PASS")
+        
+        elif(RTH == "END"):
+            break
+
+
+# In[30]:
+
+
+def Battle1(character, monster):
+    accuracy = rollToHit(character)
+    if accuracy >= monster.AC:
+        attack = attack1(character) + weapon(character) + PlayerClassDamage(character)
+        return attack
+    elif accuracy < monster.AC:
+        return 0
+    
+
+
+# In[31]:
+
+
+import random
+def weapon(character):
+    print("Swung Club")
+    if(character.weapon == "Club"):
+        club = random.randint(1,6)
+        return club
+
+
+# In[32]:
+
+
+def attack1(character):
+    if(character.Class == "Fighter"):
+        return fighterAttack(character)
+    elif(character.Class == "Rogue"):
+        return rogueAttack(character)
+
+
+# In[33]:
+
+
+def fighterAttack(character):
+    if character.strength == 11:
+        return 1
+    elif character.strength == 12:
+        return 2
+    elif character.strength == 13:
+        return 3
+    elif character.strength == 14:
+        return 5
+    elif character.strength == 15:
+        return 7
+    elif character.strength == 16:
+        return 10 
+    elif character.strength == 17:
+        return 13
+    elif character.strength == 18:
+        return 14
+    
+def rogueAttack(character):
+    if character.dexterity == 11:
+        return 1
+    elif character.dexterity == 12:
+        return 2
+    elif character.dexterity == 13:
+        return 3
+    elif character.dexterity == 14:
+        return 5
+    elif character.dexterity == 15:
+        return 7
+    elif character.dexterity == 16:
+        return 10 
+    elif character.dexterity == 17:
+        return 13
+    elif character.dexterity == 18:
+        return 14
+
+
+# In[34]:
+
+
+def PlayerClassDamage(x):
+    if(x.Class == "Fighter"):
+        return ClassFighterDamage(x)
+        print("called functionfighter")
+    if(x.Class == "Rogue"):
+        return ClassRougeDamage(x)
+        print("called functionrogue")
+
+
+# In[35]:
+
+
+
+def ClassFighterDamage(x):
+    print("in classfighter")
+    if(x.Class == "Fighter"):
+        if(x.level == 1):
+            f.BaseAttackBonus = 1
+        elif(x.level == 2):
+            f.BaseAttackBonus = 2
+        elif(x.level == 3):
+            f.BaseAttackBonus = 3
+        elif(x.level == 4):
+            f.BaseAttackBonus = 4
+        elif(x.level == 5):
+            f.BaseAttackBonus = 5
+        elif(x.level == 6):
+            f.BaseAttackBonus = 6
+            #suppose to add multiple hits
+        elif(x.level == 7):
+            f.BaseAttackBonus = 7
+        elif(x.level == 8):
+            f.BaseAttackBonus = 8
+        elif(x.level == 9):
+            f.BaseAttackBonus = 9
+        elif(x.level == 10):
+            f.BaseAttackBonus = 10
+        elif(x.level == 11):
+            f.BaseAttackBonus = 11
+        elif(x.level == 12):
+            f.BaseAttackBonus = 12
+        elif(x.level == 13):
+            f.BaseAttackBonus = 13
+        elif(x.level == 14):
+            f.BaseAttackBonus = 14
+        elif(x.level == 15):
+            f.BaseAttackBonus = 15
+        elif(x.level == 16):
+            f.BaseAttackBonus = 16
+        elif(x.level == 17):
+            f.BaseAttackBonus = 17
+        elif(x.level == 18):
+            f.BaseAttackBonus = 18 
+        elif(x.level == 19):
+            f.BaseAttackBonus = 19
+        elif(x.level == 20):
+            f.BaseAttackBonus = 20
+        return f.BaseAttackBonus
+    
+def ClassRougeDamage(x):
+    print("is a rogue")
+    if(x.Class == "Rogue"):
+        if(x.level == 1):
+            r.BaseAttackBonus = 0
+        elif(x.level == 2):
+            r.BaseAttackBonus = 1
+        elif(x.level == 3):
+            r.BaseAttackBonus = 2
+        elif(x.level == 4):
+            r.BaseAttackBonus = 3
+        elif(x.level == 5):
+            r.BaseAttackBonus = 3
+        elif(x.level == 6):
+            r.BaseAttackBonus = 4
+        elif(x.level == 7):
+            r.BaseAttackBonus = 5
+        elif(x.level == 8):
+            r.BaseAttackBonus = 6
+        elif(x.level == 9):
+            r.BaseAttackBonus = 6
+        elif(x.level == 10):
+            r.BaseAttackBonus = 7
+        elif(x.level == 11):
+            r.BaseAttackBonus = 8
+        elif(x.level == 12):
+            r.BaseAttackBonus = 9
+        elif(x.level == 13):
+            r.BaseAttackBonus = 9
+        elif(x.level == 14):
+            r.BaseAttackBonus = 10
+        elif(x.level == 15):
+            r.BaseAttackBonus = 11
+        elif(x.level == 16):
+            r.BaseAttackBonus = 12 
+        elif(x.level == 17):
+            r.BaseAttackBonus = 12
+        elif(x.level == 18):
+            r.BaseAttackBonus = 13
+        elif(x.level == 19):
+            r.BaseAttackBonus = 14
+        elif(x.level == 20):
+            r.BaseAttackBonus = 15
+        return r.BaseAttackBonus
+
+
+# In[36]:
+
+
+def monBattle(character,monster):
+    accuracy = rollToHitMon(monster)
+    newAC = character.AC + feats(character)
+    if accuracy >= newAC:
+        print("THIS IS NEW AC",newAC)
+        attack = attackMon(monster)
+        return attack
+    elif accuracy < newAC:
+        return 0
+        
+  
+        
+
+
+# In[37]:
+
+
+def rollToHitMon(monster):
+    accuracy = random.randint(1,20)
+#    prof = proficiencyMon(character)
+    
+    return accuracy 
+
+
+# In[38]:
+
+
+def attackMon(monster):
+    return monster.CR
+
+
+# In[39]:
+
+
+def feats(character):
+    if(c.feature == 'Dodge'):
+        return dodgeFeature()
+    else:
+        return 0
+    
+
+
+# In[40]:
+
+
+def dodgeFeature():
+    print("In dodgeFeature")
+    return 1
+
+
+# In[41]:
+
+
+Battle(c,m)
+
+
+# In[42]:
+
+
+Battle(r,m)
+
+
+# In[ ]:
+
+
+
+
